7e44e7a5487b7bf79fd00138fe2d2430c2b8725a
Matthew "Juniper" Barlett <emeraldinspirations+asusrog839h+u1904@gmail.com>
2019-07-22 23:19:32 -0500

    AUTO-COMMIT: Test Suite Passes

---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+// src/lib.rs
+
+// extern crate offset_calculator;
+
+use chrono::Utc;
+use chrono::Date;
+use chrono::offset::TimeZone;
+
+pub fn main (
+  offset: i32,
+  date: Date<Utc>,
+  mut writer: impl std::io::Write
+) {
+  writeln!(writer, "Hello, world!");
+}
+
+///# User Story #1563275034
+///
+///As a *[Technician] in the [Hospital Services Department]*
+///I want to be able to *enter a number of days*
+///and *have the future expiration date returned in the format mmm dd*
+#[test]
+pub fn test_user_story_1863275034() {
+  let date = Utc.ymd(1984, 5, 7);
+  let offset = 7;
+  let expected = "Expires:\t14 MAY 1984";
+  //let mut vec = Vec::new();
+  let mut actual:Vec<u8> = Vec::new();
+
+  main(offset, date, &mut actual);
+
+//  assert!(String::from_utf8_lossy(&actual).contains(expected));
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use chrono::Utc;
+use chrono::Date;
+
+mod lib;
+
 fn main() {
-    println!("Hello, world!");
+//    let args = Cli::from_args();
+//    let content = std::fs::read_to_string(&args.path)
+//        .with_context(|_| format!("could not read file `{}`", args.path.display()))?;
+
+//    lib.main(&content, &args.pattern, &mut std::io::stdout());
+
+//    Ok(())
 }
