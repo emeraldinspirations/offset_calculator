09efb0a2e9f9453e51e46e782f93d31cbe27134e
Matthew "Juniper" Barlett <emeraldinspirations+asusrog839h+u1904@gmail.com>
2019-07-24 19:31:13 -0500

    Pass: User Story #1563275034

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,32 @@
+#[macro_use]
+extern crate clap;
+
 use chrono::Utc;
 use std::env;
-use clap::App;
+use clap::{App, Arg};
 
 mod lib;
 
 fn main() {
   let matches = App::new("This is a test")
+    .author(crate_authors!())
+    .about("Calculates expiration dates of blood components based on a \
+      supplied offset and the anticoagulant type")
+    .version(crate_version!())
+    .arg(Arg::with_name("offset")
+      .help("Number of days from today to offset the calculation"))
     .get_matches();
 
-  lib::main(&0i64, Utc::today(), &mut std::io::stdout());
+  if let Some(offset) = matches.value_of("offset") {
+    lib::main(&offset.parse::<i64>().unwrap(), Utc::today(), &mut std::io::stdout());
+
+    //if let Some(offset_value) = offset.parse::<i64>() {
+    //  lib::main(&offset_value, Utc::today(), &mut std::io::stdout());
+    //} else {
+    //  println!("offset must be a 64 bit intiger");
+  } else {
+    lib::main(&0i64, Utc::today(), &mut std::io::stdout());
+  }
 
 //    Ok(())
 }
